d38d2d2818fd8b775eceade832956cabd274a58b
Jero <133540912+Jeronimojbc@users.noreply.github.com>
2024-11-05 20:52:33 -0300

    Update main.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,15 +1,29 @@
-import '../css/style.css'
-import { getCEPData} from "./fetchCEP.js";
+import '../css/style.css';
+import { getCEPData } from "./fetchCEP.js";
 
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>Qual seu CEP?</h1>
     <div>
-        <input type="text" name="cep" id="cep_field">
+        <input type="text" name="cep" id="cep_field" placeholder="Digite o CEP">
         <button type="submit" id="cep_get">GET</button>
     </div>
     <h2 id="cep">-</h2>
   </div>
-`
+`;
 
-getCEPData(document.querySelector('#cep'))
+const cepField = document.querySelector('#cep_field');
+const cepButton = document.querySelector('#cep_get');
+const cepDisplay = document.querySelector('#cep');
+
+cepButton.addEventListener('click', () => {
+  const cepValue = cepField.value.trim(); // Obtenemos el valor del campo input
+
+  if (cepValue) {
+    // Llamamos a la función getCEPData y le pasamos el valor del campo cep
+    getCEPData(cepValue, cepDisplay);
+  } else {
+    // Si no hay valor, mostramos un mensaje de advertencia
+    cepDisplay.textContent = "Por favor, insira um CEP válido!";
+  }
+});
